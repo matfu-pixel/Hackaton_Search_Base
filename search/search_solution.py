a98d0beb369c08b8d92686acc17b4699e16bcc11
matfu-pixel <matfu21@ya.ru>
2022-04-17 03:14:30 +0300

    new3

--- search/search_solution.py
+++ search/search_solution.py
@@ -1,58 +1,127 @@
-import numpy as np
-from .search import Base
-from typing import List, Tuple
-import faiss
 import pickle, os, time, gdown
 import numpy as np
+import nmslib
 from tqdm import tqdm
+from typing import List, Tuple
 from config import Config as cfg 
+from .search import Base
 
- 
+
 class SearchSolution(Base):
- 
-    def __init__(self, data_file='./data/train_data.pickle',
+    ''' SearchBase class implements 
+    search through the database to find matching
+    vector for query vector. It measures
+    search speed and assign a score based
+    on the search time and correctness of the
+    search 
+    '''
+    # @profile
+    def __init__(self, data_file='./data/train_data.pickle', 
                  data_url='https://drive.google.com/uc?id=1D_jPx7uIaCJiPb3pkxcrkbeFcEogdg2R') -> None:
+        '''
+        Creates regestration matrix and passes 
+        dictionary. Measures baseline speed on
+        a given machine
+        '''
+        print('reading from pickle')
+    
+        self.index = nmslib.init(method='hnsw', space='l2') 
+        #self.index.loadIndex('index.bin', load_data=True)
+
         self.data_file = data_file
         self.data_url = data_url
- 
+
+    # @profile
     def set_base_from_pickle(self):
+        '''
+        Downloads the data, if it does not exist.
+        Sets reg_matrix and pass_dict
+
+        reg_matrix : np.array(N, 512)
+        pass_dict : dict -> dict[idx] = [np.array[1, 512]]
+        '''
         if not os.path.isfile(self.data_file):
             if not os.path.isdir('./data'):
-                os.mkdir('./data')
-            gdown.download(self.data_url, self.data_file, quiet=False)
- 
+                os.mkdir('./data') 
+            gdown.download(self.data_url, self.data_file, quiet=False) 
+
         with open(self.data_file, 'rb') as f:
-            data = pickle.load(f)
- 
-        self.reg_matrix = [None] * len(data['reg'])
-        self.ids = {}
+            data = pickle.load(f) 
+    
+        self.reg_matrix = [np.float64] * len(data['reg'])
+        self.ids = {} 
         for i, key in enumerate(data['reg']):
             self.reg_matrix[i] = data['reg'][key][0][None]
             self.ids[i] = key
- 
-        self.reg_matrix = np.concatenate(self.reg_matrix, axis=0)
-        self.pass_dict = data['pass']
- 
-        dim = 512
-        self.index = faiss.index_factory(dim, "IVF1000,Flat", faiss.METRIC_INNER_PRODUCT)
-        print(self.index.is_trained)
-        self.index.train(self.reg_matrix.astype('float32'))
- 
-        print(self.index.is_trained)
-        print(self.index.ntotal)
-        self.index.add(self.reg_matrix.astype('float32'))
-        print(self.index.ntotal)
- 
-    def search(self, query: np.array) -> List[Tuple]:
-        #print(query)
-        self.index.nprobe = 200
-        tmp = np.zeros((1, len(query)), dtype='float32')
-        tmp[0] = np.array(query)
-        D, I = self.index.search(tmp, 5)
-        return [(I[0][i], D[0][i]) for i in range(len(D))]
- 
-    def insert_base(self, feature: np.array) -> None:
-        self.index.add(feature)
 
-    def cos_sim():
-        pass
+        self.reg_matrix = np.concatenate(self.reg_matrix, axis=0)
+
+        self.index.addDataPointBatch(self.reg_matrix)
+        index_time_params = {'M' : 186, 'indexThreadQty': 16, 'efConstruction': 142, 'post' : 2}
+        self.index.createIndex(index_time_params, print_progress=True)
+        #self.index.saveIndex('index.bin', save_data=True)
+
+        self.pass_dict = data['pass'] # index -> [vec1, vec2, vec3, ...]
+         
+        
+    
+    # @profile
+    def cal_base_speed(self, base_speed_path='./base_speed.pickle') -> float:
+        '''
+        Validates baseline and improved searh
+        Return:
+                metric : float - score for search
+        ''' 
+
+        samples = cfg.samples 
+        N, C, C_time, T_base = 0, 0, 0, 0
+        for i, tup in enumerate(tqdm(self.pass_dict.items(), total=samples)):
+
+            idx, passes = tup
+            for q  in passes:
+                t0 = time.time()
+                c_output = self.search(query=q) # [i1, i2, ..]
+                t1 = time.time()
+                T_base += (t1 - t0)
+
+                C_set = [True for tup in c_output if tup[0] == idx]
+                if len(C_set):
+                    C += 1
+                    C_time += (t1 - t0) 
+                N += 1
+
+            if i > samples:
+                break
+        print("!", N)
+
+        base_speed = T_base / N
+        print(f"Base Line Speed: {base_speed}")
+        print(f"Base Line Accuracy: {C / N * 100}")
+        #with open(base_speed_path, 'wb') as f:
+        #    pickle.dump(base_speed, f)
+
+    # @profile
+    def search(self, query: np.array) -> List[Tuple]:
+        '''
+        Arguments:
+            query : np.array - 1x512
+        Return:
+            List[Tuple] - indicies of search, similarity
+        '''
+        #print(query.shape, " ", type(query[0]))
+        #self.index.loadIndex('index.bin', load_data=True)
+        return self.index.knnQuery(query, k = 566)
+            
+
+
+    
+
+    def insert_base(self, feature: np.array) -> None:
+
+        ## there no inplace concationation in numpy so far. For inplace
+        ## concationation operation both array should be contingious in 
+        ## memory. For now, let us suffice the naive implementation of insertion
+        self.reg_matrix = np.concatenate(self.reg_matrix, feature, axis=0) 
+
+    def cos_sim(self, query: np.array) -> np.array:
+        return np.dot(self.reg_matrix, query)
